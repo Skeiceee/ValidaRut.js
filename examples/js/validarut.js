e85174a8ed426ba503a0fd9c1e9d57cdd7d73c6e
Victor Nuñez <skeiceee@gmail.com>
2019-02-09 15:11:48 -0300

    Update validarut.js

--- examples/js/validarut.js
+++ examples/js/validarut.js
@@ -1,12 +1,11 @@
 $.fn.ValidaRut = function() {
 	var validacion = false
-	var valor = this.val().trim().replace('.', '')
+	var valor = this.val().replace(/[.]/g, '').trim()
 
 	valor = valor.replace('-', '')
 	var cuerpo = valor.slice(0, -1)
 	dv = valor.slice(-1).toUpperCase()
-	var m = 0,
-	    s = 1;
+	var m = 0, s = 1;
 	for (; cuerpo; cuerpo = Math.floor(cuerpo / 10)) {
 	    s = (s + cuerpo % 10 * (9 - m++ % 6)) % 11
 	}
@@ -14,5 +13,5 @@ $.fn.ValidaRut = function() {
 	if (dv == (s ? s - 1 : 'K'))
 	    validacion = true
 
-	return (valor === '' || valor.slice(0, -1) < 7) ? false : validacion;
+	return (valor === '' || valor.slice(0, -1) < 7 || isNaN(s)) ? false : validacion;
 }
